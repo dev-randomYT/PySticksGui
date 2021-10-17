010eaea0450a193e7bbdac313958dca706d0f4b8
Adrian <adrian@zivkovic.com.au>
2021-10-17 13:19:18 +1100

    Need to Convert Str To Float everything else works

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import keyboard
 
 from pysticks import *
 
+con = get_controller()
 
+throttle = con.getThrottle()
+yaw = con.getYaw()
+pitch = "Pitch: %+2.2f" % con.getPitch()
+roll = con.getPitch
 
+#Initiate PyGame
 pygame.init()
 
 color = (123, 123, 123)
 
-con = get_controller()
-
+#Main Loop
 while True:
     surface = pygame.display.set_mode((800, 600))
 
@@ -29,3 +34,6 @@ while True:
     except KeyboardInterrupt:
 
         break
+
+    pygame.draw.rect(surface, color, pygame.Rect(float(100, 100, 150 + float(pit), 105)))
+    pygame.display.flip()
