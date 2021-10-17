c7f344dd875939adff187871e35fd2a88dfe4ac9
Adrian <adrian@zivkovic.com.au>
2021-10-17 11:47:41 +1100

    Imported the values from pysticks into main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 import keyboard
 
+from pysticks import *
+
+
+
 pygame.init()
 
 color = (123, 123, 123)
+
+con = get_controller()
 
 while True:
     surface = pygame.display.set_mode((800, 600))
@@ -12,3 +18,14 @@ while True:
             pygame.display.quit()
             print('Bye :)')
             break
+
+    try:
+
+            con.update()
+
+            print('Throttle: %+2.2f   Roll: %+2.2f   Pitch: %+2.2f   Yaw: %+2.2f' %
+                (con.getThrottle(), con.getRoll(), con.getPitch(), con.getYaw()))
+
+    except KeyboardInterrupt:
+
+        break
