b4ac87138ad4845fe729b43b7ae31a734cb56d07
Adrian <adrian@zivkovic.com.au>
2021-10-31 10:55:39 +1100

    Need to add text

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ A simple work in progress gui based off the pysticks library by simondlevy. Stil
 
 ## Documentation
 ---
-* To First Run This Program open CMD. Then navigate to your folder. Change you directory to PySticks. Then tpye "python joyreporter.py" this will show all the buttons and axis on you controller.
+* To First Run This Program open CMD. Then navigate to your folder. Change you directory to PySticks. Then tpye "python joyreporter.py" this will show all the buttons and axis on you controller*
+* The you need to wait for the main release to start.
 
 ## To Do
 ---
-* Needs Sliders that corrispond to the values out putted by the analog controller.
-* Needs A Gui Based on PYGAME libary.
+* Needs Sliders that corrispond to the values out putted by the analog controller. (Done)
+* Needs A Gui Based on PYGAME libary. (Done)
 * Needs A Easy Config That Isn't Trash.
 * Needs Config Tuning For OBS.
-* Need to figure out how to import the values.
-* Need to begin the main GUI
+* Need to figure out how to import the values. (Done)
+* Need to begin the main GUI (Done)
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import pygame
 import keyboard
 
+#import pystick libary
 from pysticks import *
 
+#define con
 con = get_controller()
-
-throttle = con.getThrottle()
-yaw = con.getYaw()
-pitch = float("%+2.2f" % con.getPitch())
-roll = con.getPitch
 
 #Initiate PyGame
 pygame.init()
 
+#defining
 color1 = ('blue')
 color2 = ('purple')
 color3 = ('red')
 color4 = ('orange')
+colortxt = ('gray')
 
 text = (0, 0, 0)
 
@@ -31,26 +30,34 @@ while True:
 
     surface = pygame.display.set_mode((800, 600))
 
+
+    #Yaw
     if yaw < 0:
         yaw = yaw + yaw * -2
 
         #draw the rectangles with dyamic sizes w1, h1, w2, h2
     pygame.draw.rect(surface, color1, pygame.Rect(100, 225, 600 * yaw, 50))
+    pygame.draw.rect(surface, colortxt, pygame.Rect(110, 225, 175, 150))
 
-
+    #Thorttle
     #draw the rectangles with dyamic sizes w1, h1, w2, h2
     pygame.draw.rect(surface, color2, pygame.Rect(100, 125, 600 * throttle, 50))
+    pygame.draw.rect(surface, colortxt, pygame.Rect(100, 125, 175, 150))
 
+    #Roll
     if roll < 0:
         roll = roll + roll * -2
 
     pygame.draw.rect(surface, color3, pygame.Rect(100, 325, 600 * roll, 50))
+    pygame.draw.rect(surface, colortxt, pygame.Rect(100, 325, 175, 150))
 
+    #Pitch
     if pitch < 0:
         pitch = pitch + pitch * -2
 
     #draw the rectangles with dyamic sizes w1, h1, w2, h2
     pygame.draw.rect(surface, color4, pygame.Rect(100, 425, 600 * pitch, 50))
+    pygame.draw.rect(surface, colortxt, pygame.Rect(100, 425, 175, 150))
     pygame.display.flip()
 
     if keyboard.is_pressed('ctrl+q'):
